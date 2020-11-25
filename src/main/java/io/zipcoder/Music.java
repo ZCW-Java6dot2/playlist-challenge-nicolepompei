219d5ddb835f2e07eee232a6e810cf267fb43778
pompy <nicole.lynn.pompei@gmail.com>
2020-11-24 19:29:41 -0500

    a significantly better answer

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -14,31 +14,53 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection) {
-        ArrayList<String> playlistArrayList = new ArrayList<String>(Arrays.asList(playList));
-        ArrayList<String> tempList = new ArrayList<String>();
-        tempList.addAll(playlistArrayList);
-        tempList.addAll(playlistArrayList);
-        tempList.addAll(playlistArrayList);
 
-        Integer countForward = 0;
-        Integer countBackward = 0;
-        int length = playlistArrayList.size() + startIndex;
-
-        for (int i = length; i < tempList.size(); i++) {
-            if (tempList.get(i) == selection) {
-                break;
+        int startIndexBackwards = startIndex;
+        int forwardCount = 0;
+        int backwardCount = 0;
+        while(!playList[startIndex].equals(selection)) {
+            startIndex++;
+            forwardCount++;
+            if(startIndex >= playList.length) {
+                startIndex = 0;
             }
-            countForward++;
         }
-
-        for (int i = length; i > 0; i--) {
-            if (tempList.get(i) == selection) {
-                break;
+        while(!playList[startIndexBackwards].equals(selection)) {
+            startIndexBackwards--;
+            backwardCount++;
+            if(startIndexBackwards < 0) {
+                startIndexBackwards = playList.length - 1;
             }
-            countBackward++;
         }
+        return (Math.min(forwardCount, backwardCount));
 
-        return Math.min(countForward, countBackward);
 
     }
+//        ArrayList<String> playlistArrayList = new ArrayList<String>(Arrays.asList(playList));
+//        ArrayList<String> tempList = new ArrayList<String>();
+//        tempList.addAll(playlistArrayList);
+//       tempList.addAll(playlistArrayList);
+//        tempList.addAll(playlistArrayList);
+//
+//        Integer countForward = 0;
+//        Integer countBackward = 0;
+//        int length = playlistArrayList.size() + startIndex;
+//
+//        for (int i = length; i < tempList.size(); i++) {
+//            if (tempList.get(i) == selection) {
+//                break;
+//            }
+//            countForward++;
+//        }
+//
+//        for (int i = length; i > 0; i--) {
+//            if (tempList.get(i) == selection) {
+//                break;
+//            }
+//            countBackward++;
+//        }
+//
+//        return Math.min(countForward, countBackward);
+//
+//    }
 }
